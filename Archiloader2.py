00fb536cc5945e00a757f66c8902c1dd2da63b59
therezien titouan <titouan.therezien@edu.univ-fcomte.fr>
2024-03-11 16:01:49 +0100

    j'ai refait un loader, mais j'ai l'impression que ce n'est pas encore parfait

--- Archiloader2.py
+++ Archiloader2.py
@@ -21,31 +21,55 @@ def loaderData():
 
     df = [df2009, df2010, df2011, df2012, df2013, df2014, df2015, df2016, df2017, df2018, df2019, df2020, df2021,
           df2022, df2023, df2024]
+
     for frame in df:
         print(frame.shape)
+
 
     df2023_2024 = pd.DataFrame()
     df2019_2024 = pd.DataFrame()
     df2009_2024 = pd.DataFrame()
+
+    list_of_2009_2024 = []
+    list_of_2019_2024 = []
+    list_of_2023_2024 = []
+
     columns_to_drop = ['viking_flow', 'scottish_transfer', 'nsl_flow', 'eleclink_flow']
     columns_to_drop2 = ['viking_flow', 'scottish_transfer']
     columns_to_drop3 = ['nsl_flow', 'eleclink_flow']
 
-    for frame in df:
-        frame = frame.rename(columns=str.lower)
-        frame['settlement_date'] = pd.to_datetime(frame['settlement_date'], errors='coerce')
-        if all(col in frame.columns for col in columns_to_drop):
-            df2023_2024 = pd.concat([df2023_2024, frame], ignore_index=True)
-        if all(col in frame.columns for col in columns_to_drop3):
-            df2019_2024 = pd.concat([df2023_2024, frame], ignore_index=True)
-        df2009_2024 = pd.concat([df2023_2024, frame], ignore_index=True)
+    for frame in range(0,len(df)):
+        print(frame)
+        df[frame] = df[frame].rename(columns=str.lower)
+        df[frame]['settlement_date'] = pd.to_datetime(df[frame]['settlement_date'], errors='coerce')
+
+
+    for frame in range(0,len(df)):
+        if all(col in df[frame].columns for col in columns_to_drop):
+            list_of_2023_2024.append(df[frame])
+        if all(col in df[frame].columns for col in columns_to_drop3):
+            list_of_2019_2024.append(df[frame])
+        list_of_2009_2024.append(df[frame])
+
+    print("list_of_2009_2024", len(list_of_2009_2024))
+    print("list_of_2019_2024", len(list_of_2019_2024))
+    print("list_of_2023_2024", len(list_of_2023_2024))
+
+    df2009_2024 = pd.concat(list_of_2009_2024, ignore_index=True)
+    df2019_2024 = pd.concat(list_of_2019_2024, ignore_index=True)
+    df2023_2024 = pd.concat(list_of_2023_2024, ignore_index=True)
 
     df2019_2024 = df2019_2024.drop(columns=columns_to_drop2, errors='ignore')
     df2009_2024 = df2009_2024.drop(columns=columns_to_drop, errors='ignore')
 
-    print("df2009_2024.shape", df2009_2024.shape)
-    print("df2019_2024.shape", df2019_2024.shape)
-    print("df2023_2024.shape", df2023_2024.shape)
+    print("df2009_2024.shape , expected : 280320   =>", df2009_2024.shape)
+    print("df2019_2024.shape , expected : 88012    =>", df2019_2024.shape)
+    print("df2023_2024.shape , expected : 18384    =>", df2023_2024.shape)
 
-    return [df2009_2024, df2019_2024, df2023_2024]
 
+
+
+
+
+
+loaderData()
--- VericationData.py
+++ VericationData.py
@@ -9,6 +9,13 @@ df2009_2024 = dfs[0]
 df2019_2024 = dfs[1]
 df2023_2024 = dfs[2]
 
+print("demarage de la verification")
 
 
-print(df2009_2024['settlement_date'].head(5))
+for value in df2009_2024['non_bm_stor']:
+    if value != 0:
+        print("La valeur est différente de 0", value)
+
+print(df2009_2024.shape)
+
+
